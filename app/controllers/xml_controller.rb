f94e2465c4b25ba5f054d7dd745e4f3621a111a6
ayato_p <ayato.skycrawlers@gmail.com>
2013-11-13 00:13:34 +0900

    rss

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -1,4 +1,10 @@
 class XmlController < ApplicationController
   def rss
+    request.format = "xml"
+    respond_to do |format|
+      @feed_title = "ayalog"
+      @posts = Post.all.order("created_at desc").limit(10)
+      format.xml {render :layout => false}
+    end
   end
 end
